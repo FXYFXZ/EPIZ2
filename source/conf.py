11b3e8cc88f959dcb3c2398e606596df74a192a2
Valery <vfilin@mail.ru>
2025-10-18 11:33:06 +0300

    Import

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'RD'
+copyright = '2025, Valery@EP'
+author = 'Valery@EP'
+release = 'IZ2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+rst_epilog = """
+.. |product_name| replace:: регистратор диаметра
+.. |prd| replace:: РД
+.. |company| replace:: EP
+.. |release_date| replace:: 2025
+"""
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
